30f7bf9c65ef28780846d0a79a6c3e2f0c90a488
adminhype <ado.designs@outlook.de>
2025-02-02 23:16:56 +0100

    Update Project

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function calc(){
-    let euro = euroField.value;
-    let result = euro * 0.981;
-    usd.value = result.toFixed(2).replace('.', ',');
+// Zugriff auf die DOM-Elemente
+const euroField = document.getElementById("euroField");
+const usdField = document.getElementById("usd");
+
+// Funktion zur Umrechnung von Euro in USD
+function calc() {
+  let euro = parseFloat(euroField.value);
+  let result = euro * 0.981;
+  usdField.value = result.toFixed(2);
+}
+
+// Funktion zur Umrechnung von USD in Euro
+function calc2() {
+  let usd = parseFloat(usdField.value);
+  let result = usd * 1.12;
+  euroField.value = result.toFixed(2);
 }
